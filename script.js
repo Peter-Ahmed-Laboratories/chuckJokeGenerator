49d0614a3ceda28b2a7671f9a40d070cc97d0a74
cabbage <peterdelmastro@hotmail.com>
2021-04-04 21:45:38 -0400

    centered background

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 // init
 
-      // make initial query for joke
-      const jokeQuery = () => {
-        fetch("http://api.icndb.com/jokes/random?limitTo=[nerdy]")
-          .then((response) => {
-            return response.json();
-          })
-          .then((jsonResponse) => {
-               
-            //clear jokefield
-            document.querySelector('#jokeResponse').innerHTML = '';
-            
-            // display joke
-            document.querySelector('#jokeResponse').append(jsonResponse.value.joke);
-           
-        
-              
-          });
-     
-      };
+// make initial query for joke
+const jokeQuery = () => {
+  fetch("http://api.icndb.com/jokes/random?limitTo=[nerdy]")
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      //clear jokefield
+      document.querySelector("#jokeResponse").innerHTML = "";
 
-     
+      // filter joke for "&quot;"
+      const filteredJoke = jsonResponse.value.joke.replace(/&quot;/g, '"');
 
-      // listen for button click for new joke
-// var quotFix = () => {
-//   document.body.innerHTML = document.body.innerHTML.replace('&quot;', '"');
-// }
+      // display joke
+      document.querySelector("#jokeResponse").append(filteredJoke);
+    });
+};
 
+// listen for button click for new joke
